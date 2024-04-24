cad3cafa3ac210c83ba2ceaab5509fa1ef1bcb19
David Quintino <davidkintino@gmail.com>
2024-04-24 12:33:24 +0100

    input

--- input.py
+++ input.py
@@ -1,1 +1,17 @@
-import numpy
+import sys
+import numpy 
+
+rows = []
+line = sys.stdin.readline().split()
+i = len(line)
+
+while(i>0):
+  
+
+    rows.append(numpy.array(line))
+    line = sys.stdin.readline().split()  
+    i-=1
+
+array_2D = numpy.stack(rows)
+
+print(array_2D)
